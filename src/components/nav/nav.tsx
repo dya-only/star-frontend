797e5c6d57878448e542b78f499909327619b466
dya-only <dyacode@proton.me>
2023-12-02 00:07:03 +0900

    feat: login signup window

--- src/components/nav/nav.tsx
+++ src/components/nav/nav.tsx
@@ -1,7 +1,16 @@
-import { useEffect } from 'react'
+import { ChangeEvent, Fragment, useEffect, useState } from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faRightToBracket } from '@fortawesome/free-solid-svg-icons'
 import style from './nav.module.css'
 
 export default function Nav() {
+  const [login, setLogin] = useState<boolean>(false)
+  const [signup, setSignup] = useState<boolean>(false)
+  const [user, setUser] = useState({
+    email: '',
+    password: ''
+  })
+
   useEffect(() => {
     const nav = document.querySelector('nav')!
 
@@ -24,16 +33,54 @@ export default function Nav() {
     })
   }, [])
 
+  const windowController = () => {
+    if (login) {
+      const body = document.querySelector('body')!
+      body.style.overflowY = 'auto'
+      setLogin(false)
+
+      return
+    }
+
+    const body = document.querySelector('body')!
+    body.style.overflowY = 'hidden'
+    setLogin(true)
+  }
+
+  const submitLogin = () => {
+    setLogin(false);
+    window.location.href = '/'
+  }
+
   return (
-    <nav>
-      <div className={style.container}>
-        <a className={style.navTitle} href="/">Star</a>
-        <div className={style.navItems}>
-          <div className={style.navItem}>메인</div>
-          <div className={style.navItem}>랭킹</div>
-          <div className={style.navItem}>비교</div>
+    <Fragment>
+      { login ?
+        <div className={login ? style.windowContainer : style.windowContainerNormal}>
+          <div className={style.window}>
+            <form onSubmit={submitLogin}>
+              <input type="text" className={style.windowInput} placeholder='dyacode@pmh.codes' value={user.email} onChange={(e: ChangeEvent<HTMLInputElement>) => setUser({ ...user, email: e.target.value })} />
+              <input type="password" className={style.windowInput} placeholder='********' value={user.password} onChange={(e: ChangeEvent<HTMLInputElement>) => setUser({ ...user, password: e.target.value })} />
+              <button className={style.windowButton} onClick={submitLogin}>로그인</button>
+              <div className={style.windowWhy} onClick={() => {  setSignup(true); setLogin(false) }}>회원가입 하러가기</div>
+            </form>
+
+          </div>
         </div>
-      </div>
-    </nav>
+      : null }
+
+      <nav>
+        <div className={style.container}>
+          <a className={style.navTitle} href="/">Star</a>
+          <div className={style.navItems}>
+            <div className={style.navItem}>메인</div>
+            <div className={style.navItem}>랭킹</div>
+            <div className={style.navItem}>비교</div>
+            <div className={style.navItem} onClick={windowController}>
+              <FontAwesomeIcon icon={faRightToBracket} className={style.icon} />
+            </div>
+          </div>
+        </div>
+      </nav>
+    </Fragment>
   )
 }
