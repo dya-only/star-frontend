2d4c3696b53afd64a01745c1c872e1a0977ac0bd
Boseok <dyacode@pmh.codes>
2023-12-11 11:25:08 +0900

    feat: update profile

--- src/pages/profile/profile.tsx
+++ src/pages/profile/profile.tsx
@@ -1,4 +1,4 @@
-import { Fragment, useEffect, useState } from 'react'
+import { ChangeEvent, ChangeEventHandler, FormEvent, Fragment, HTMLInputTypeAttribute, useEffect, useState } from 'react'
 import style from './profile.module.css'
 import { useNavigate, useParams } from 'react-router-dom'
 import axios from 'axios'
@@ -7,14 +7,20 @@ import Nav from '../../components/nav/nav'
 export default function Profile() {
   const { githubid } = useParams()
   const navigate = useNavigate()
-  
+
   const [user, setUser] = useState({
     id: 0,
     email: '',
     githubId: '',
     image: ''
   })
+  const [update, setUpdate] = useState({
+    email: '',
+    password: '',
+    githubId: ''
+  })
   const [isMyProfile, setIsMyProfile] = useState<boolean>(false)
+  const [isUpdateWindow, setIsUpdateWindow] = useState<boolean>(false)
 
   useEffect(() => {
     verify()
@@ -62,19 +68,62 @@ export default function Profile() {
     navigate(`/vs/${user.githubId}`)
   }
 
+  const updateSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+
+    const formData = new FormData()
+    if (update.email !== '') formData.append('email', update.email)
+    if (update.password !== '') formData.append('password', update.password)
+    if (update.githubId !== '') formData.append('githubId', update.githubId)
+
+    axios.put(`/api/user/${user.id}`, formData, {
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      }
+    }).then(resp => {
+      console.log(resp.data)
+      window.location.href = `/profile/${update.githubId == '' ? user.githubId : update.githubId }`
+    })
+  }
+
   return (
     <Fragment>
       <Nav type='contain' />
 
+      {isUpdateWindow ?
+        <div className={style.updateWindowContainer}>
+          <form onSubmit={updateSubmit}>
+            <div className={style.updateWindow}>
+              <div className={style.inputContainer}>
+                <div className={style.inputTag}>새로운 이메일 (선택)</div>
+                <input type="email" className={style.input} placeholder='dyacode@proton.me' value={update.email} onChange={(e: ChangeEvent<HTMLInputElement>) => setUpdate({ ...update, email: e.target.value })} />
+              </div>
+
+              <div className={style.inputContainer}>
+                <div className={style.inputTag}>새로운 비밀번호 (선택)</div>
+                <input type="password" className={style.input} placeholder='* * * * * * * *' value={update.password} onChange={(e: ChangeEvent<HTMLInputElement>) => setUpdate({ ...update, password: e.target.value })} />
+              </div>
+
+              <div className={style.inputContainer}>
+                <div className={style.inputTag}>새로운 깃허브 아이디 (선택)</div>
+                <input type="text" className={style.input} placeholder='dya-only' value={update.githubId} onChange={(e: ChangeEvent<HTMLInputElement>) => setUpdate({ ...update, githubId: e.target.value })} />
+              </div>
+
+              <button className={style.updateBtn} type='submit'>정보 수정</button>
+            </div>
+          </form>
+        </div>
+        : null}
+
       <div className={style.profileContainer}>
         <div className={style.container}>
           <div className={style.profile}>
-            <img className={style.profileImg} src={`/api/image/user/${user.image}`} alt="" />
+            <img className={style.profileImg} src={`/api/image/user/${user.image}`} alt="" onClick={() => setIsUpdateWindow(true)} />
             <div className={style.profileContentContainer}>
-              <div className={style.profileEmail}>{ user.email }</div>
-              <a className={style.profileGithubId} href={`https://github.com/${user.githubId}`} target='_blank'>{ user.githubId }</a>
-              { isMyProfile ? <button className={style.logout} onClick={logout}>로그아웃</button> : null }
-              { !isMyProfile ? <button className={style.vs} onClick={vs}>VS</button> : null }
+              <div className={style.profileEmail}>{user.email}</div>
+              <a className={style.profileGithubId} href={`https://github.com/${user.githubId}`} target='_blank'>{user.githubId}</a>
+              {isMyProfile ? <button className={style.logout} onClick={logout}>로그아웃</button> : null}
+              {!isMyProfile ? <button className={style.vs} onClick={vs}>VS</button> : null}
             </div>
           </div>
         </div>
